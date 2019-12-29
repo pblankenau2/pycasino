f368d73a39547457a6c2ae2706c5f57525f75b0e
Philip Blankenau <pblankenau@msn.com>
2019-12-29 14:19:39 -0700

    Rename bin_builder to wheel_builder, a more accurate name.

--- pycasino/roulette/wheel_builder.py
+++ pycasino/roulette/wheel_builder.py
@@ -13,25 +13,25 @@ def get_outcome(name):
 def create_wheel():
     """Populate the bins in wheel with outcomes."""
     wheel = model.Wheel()
-    add_straight_bets(wheel)
-    add_split_bets(wheel)
-    add_column_bets(wheel)
-    add_corner_bets(wheel)
-    add_dozen_bets(wheel)
-    add_line_bets(wheel)
-    add_street_bets(wheel)
-    add_even_money_bets(wheel)
-    add_five_bets(wheel)
-    return wheel
+    _add_straight_bets(wheel)
+    _add_split_bets(wheel)
+    _add_column_bets(wheel)
+    _add_corner_bets(wheel)
+    _add_dozen_bets(wheel)
+    _add_line_bets(wheel)
+    _add_street_bets(wheel)
+    _add_even_money_bets(wheel)
+    _add_five_bets(wheel)
+    return wheel 
 
 
-def add_five_bets(wheel):
+def _add_five_bets(wheel):
     """Add five bet outcomes to the wheel."""
     wheel.add_outcome(0, Outcome('Five 00-0-1-2-3', 6))
     wheel.add_outcome(37, Outcome('Five 00-0-1-2-3', 6))
 
 
-def add_straight_bets(wheel):
+def _add_straight_bets(wheel):
     """Generate straight bet outcomes and add them to the wheel."""
     for number in range(0, 36):
         outcome = Outcome('Straight {}'.format(number), 35)
@@ -40,7 +40,7 @@ def add_straight_bets(wheel):
     wheel.add_outcome(37, Outcome('Straight 00', 35))
 
 
-def add_split_bets(wheel):
+def _add_split_bets(wheel):
     """Add split bet outcomes to the wheel."""
     for row in range(0, 11):
         for col in range(1, 3):
@@ -57,7 +57,7 @@ def add_split_bets(wheel):
             wheel.add_outcome(first_row_num + 3, outcome)
 
 
-def add_street_bets(wheel):
+def _add_street_bets(wheel):
     """Add street bet outcomes to the wheel."""
     for row in range(0, 12):
         first_col_num = 3 * row + 1
@@ -74,7 +74,7 @@ def add_street_bets(wheel):
         wheel.add_outcome(first_col_num + 2, outcome)
 
 
-def add_corner_bets(wheel):
+def _add_corner_bets(wheel):
     """Add corner bet outcomes to the wheel."""
     for row in range(0, 11):
         for col in range(1, 3):
@@ -94,7 +94,7 @@ def add_corner_bets(wheel):
             wheel.add_outcome(first_col_num + 4, outcome)
 
 
-def add_line_bets(wheel):
+def _add_line_bets(wheel):
     """Add line bet outcomes to the wheel."""
     for row in range(0, 11):
         first_col_num = 3 * row + 1
@@ -117,7 +117,7 @@ def add_line_bets(wheel):
         wheel.add_outcome(first_col_num + 5, outcome)
 
 
-def add_dozen_bets(wheel):
+def _add_dozen_bets(wheel):
     """Add dozen bet outcomes to the wheel."""
     name_mapping = {0: 'First', 1: 'Second', 2: 'Third'}
 
@@ -127,7 +127,7 @@ def add_dozen_bets(wheel):
             wheel.add_outcome(12 * dozen + num + 1, outcome)
 
 
-def add_column_bets(wheel):
+def _add_column_bets(wheel):
     """Add column bet outcomes to the wheel."""
     for column in range(0, 3):
         outcome = Outcome('Column {}'.format(column + 1), 2)
@@ -135,7 +135,7 @@ def add_column_bets(wheel):
             wheel.add_outcome(3 * row + column + 1, outcome)
 
 
-def add_even_money_bets(wheel):
+def _add_even_money_bets(wheel):
     """Add even-money bet outcomes to the wheel."""
     red_outcome = Outcome('Red', 1)
     black_outcome = Outcome('Black', 1)
