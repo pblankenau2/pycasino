945c26f36fe97280eb49e26b3866c0ee512d0387
Philip Blankenau <pblankenau@msn.com>
2020-02-17 15:41:25 -0700

    Improve CLI.

--- pycasino/cli.py
+++ pycasino/cli.py
@@ -3,39 +3,58 @@
 
 import sys
 import click
-from roulette import model
-from roulette import player as player_module
-from roulette import wheel_builder
+from .roulette import model
+from .roulette import player
+from .roulette import wheel_builder
 
 
 def player_factory(player_name, **kwargs):
     """
     kwargs will contain all the params necessary to initialize the player
     """
-    return player_module.REGISTERED_PLAYERS[player_name](**kwargs)
+    return player.REGISTERED_PLAYERS[player_name](**kwargs)
 
 
 @click.command()
-@click.option('--num-games', '-n', default=50, help='Greet as a cowboy.')
-@click.option('--player','-p',required=True, type=click.Choice(player_module.REGISTERED_PLAYERS.keys() ,case_sensitive=False))
-@click.option('--game', '-g', required=True, type=click.Choice(['roulette'], case_sensitive=False)) #TODO: See if you can make it not run if the args are missing.  Give help  instead?
+@click.option("--num-games", "-n", default=50, help="The number of games to play.")
+@click.option(
+    "--player",
+    "-p",
+    required=True,
+    type=click.Choice(player.REGISTERED_PLAYERS.keys(), case_sensitive=False),
+    help="A player will play a game with a particular betting strategy.",
+)
+@click.option(
+    "--game",
+    "-g",
+    required=True,
+    type=click.Choice(["roulette"], case_sensitive=False),
+    help="The casino game you want to play.",
+)  # TODO: See if you can make it not run if the args are missing.  Give help  instead?
 def main(game, player, num_games):
+    """Collects statistics about the outcomes of using particular betting
+    strategies in different games.  This is not a simulation where you play
+    the game yourself.  Instead, you select your game, a player that
+    will play with a certain betting strategy and the number of game sessions
+    that player should play.  A players session is limited by his/her stake
+    and a maximum number of rounds to play.  These can be modified in a 
+    configuration file.
+    
+    """
+    # TODO: Make player etc. configurable in a config file.
 
-    if game == 'roulette':
-        plyr = player_factory(player_name=player)
-        table = model.Table(limit=350)
-        wheel = wheel_builder.create_wheel()
-        game = model.Game(table=table, wheel=wheel)
-        sim = model.Simulator(
-            game=game,
-            player=plyr
+    if game == "roulette":  # TODO: Make roulette a subcommand not an option
+        game = model.Game(
+            table=model.Table(limit=350), wheel=wheel_builder.create_wheel()
         )
+        sim = model.Simulator(game=game, player=player_factory(player_name=player))
         sim.gather(samples=num_games)
-        click.echo(sim.maxima)
+        for i in sim.maxima:
+            click.echo(i)
 
     return 0
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    sys.exit(main())
 
