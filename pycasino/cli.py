1af5335bc5c8dbf3e48b3a5c0b7b9424cb5645ee
Philip Blankenau <pblankenau@msn.com>
2020-02-23 16:17:29 -0700

    Improve CLI by adding option for passing in a configuration file.

--- pycasino/cli.py
+++ pycasino/cli.py
@@ -3,6 +3,8 @@
 
 import sys
 import click
+import yaml
+import click_config_file
 from .roulette import model
 from .roulette import player
 from .roulette import wheel_builder
@@ -15,23 +17,13 @@ def player_factory(player_name, **kwargs):
     return player.REGISTERED_PLAYERS[player_name](**kwargs)
 
 
-@click.command()
-@click.option("--num-games", "-n", default=50, help="The number of games to play.")
-@click.option(
-    "--player",
-    "-p",
-    required=True,
-    type=click.Choice(player.REGISTERED_PLAYERS.keys(), case_sensitive=False),
-    help="A player will play a game with a particular betting strategy.",
-)
-@click.option(
-    "--game",
-    "-g",
-    required=True,
-    type=click.Choice(["roulette"], case_sensitive=False),
-    help="The casino game you want to play.",
-)  # TODO: See if you can make it not run if the args are missing.  Give help  instead?
-def main(game, player, num_games):
+def read_config(filepath, cmd_name):
+    with open(filepath) as config_data:
+        return yaml.safe_load(config_data)[cmd_name]
+
+
+@click.group()
+def main():
     """Collects statistics about the outcomes of using particular betting
     strategies in different games.  This is not a simulation where you play
     the game yourself.  Instead, you select your game, a player that
@@ -41,16 +33,63 @@ def main(game, player, num_games):
     configuration file.
     
     """
-    # TODO: Make player etc. configurable in a config file.
+    pass
 
-    if game == "roulette":  # TODO: Make roulette a subcommand not an option
-        game = model.Game(
-            table=model.Table(limit=350), wheel=wheel_builder.create_wheel()
-        )
-        sim = model.Simulator(game=game, player=player_factory(player_name=player))
-        sim.gather(samples=num_games)
-        for i in sim.maxima:
-            click.echo(i)
+
+@main.command()
+@click.option(
+    "--num-games",
+    "-n",
+    default=50,
+    show_default=True,
+    help="The number of games to play.",
+)
+@click.option(
+    "--stake", "-s", default=100.0, show_default=True, help="The players initial funds."
+)
+@click.option(
+    "--max-rounds",
+    default=20,
+    show_default=True,
+    help="The maximum # of rounds per game.",
+)
+@click.option(
+    "--base-bet-amount",
+    default=20.0,
+    show_default=True,
+    help="The initial bet amount (strategies modify following bets amounts).",
+)
+@click.option(
+    "--table-limit",
+    default=350.0,
+    show_default=True,
+    help="The maximum amount a bet can be.",
+)
+@click_config_file.configuration_option(provider=read_config, implicit=False)
+@click.argument(
+    "player",
+    type=click.Choice(player.REGISTERED_PLAYERS.keys(), case_sensitive=False),
+    nargs=1,
+)
+def roulette(player, stake, base_bet_amount, max_rounds, table_limit, num_games):
+
+    player_args = {
+        "stake": stake,
+        "base_bet_amount": base_bet_amount,
+        "rounds": max_rounds,
+    }
+
+    game = model.Game(
+        table=model.Table(table_limit), wheel=wheel_builder.create_wheel()
+    )
+    sim = model.Simulator(
+        game=game, player=player_factory(player_name=player, **player_args)
+    )
+    sim.gather(num_games)
+
+    click.echo("maximum_stake, rounds_played")
+    for i in zip(sim.maxima, sim.durations):
+        click.echo(f"{i[0]}, {i[1]}")
 
     return 0
 
